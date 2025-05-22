eb0059866732339442b9090591531dd6ca5ac935
dialisonveroneze <42560402+dialisonveroneze@users.noreply.github.com>
2025-05-22 16:05:39 -0300

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,11 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },  // ✅ Necessário para Supabase
-    host: 'db.tqewvjwhbepuzwpptxer.supabase.co',  // ✅ Força host
-    port: 5432,  // ✅ Força porta
+  connectionString: process.env.DATABASE_URL,
+  ssl: { 
+    rejectUnauthorized: false 
+  },
 });
+
+module.exports = pool;
