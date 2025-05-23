8c43e39acf620d555d843962bf52f26b03c9ec6d
dialisonveroneze <42560402+dialisonveroneze@users.noreply.github.com>
2025-05-22 21:41:07 -0300

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  host: 'db.tqewvjwhbepuzwpptxer.supabase.co',
+  family: 4 // <-- Força IPv4
 });
 
 module.exports = pool;
