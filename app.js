20bb9c9b7ef68ed495095af7b6b67982db1d14a3
dialisonveroneze <42560402+dialisonveroneze@users.noreply.github.com>
2025-05-26 18:12:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const fetchShopeeOrders = require('./src/jobs/fetchShopeeOrders');
 const normalizeShopee = require('./src/jobs/normalizeShopee');
 
+console.log('Iniciando aplicação...');
+
 (async () => {
   await fetchShopeeOrders();
   await normalizeShopee();
